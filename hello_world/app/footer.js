cc8162f983f26fd4f64d414c2dbb226e8a3c7d66
Lana Nelson <lana@mediasilo.com>
2015-09-10 00:08:03 -0400

    v2

---
+++ hello_world/app/footer.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Footer = React.createClass({
+
+    render: function() {
+        return (
+            <div>Powered by MediaSilo</div>
+        );
+    }
+});
+
+module.exports = Footer;
--- hello_world/app/main.js
+++ hello_world/app/main.js
@@ -1,4 +1,12 @@
 'use strict';
 var component = require('./header.js');
+var React = require('react');
+var Footer = require('./footer.js');
 
 document.body.appendChild(component());
+
+var footerContainer = document.createElement('footer');
+footerContainer.setAttribute("id", "footer");
+document.body.appendChild(footerContainer);
+
+React.render(React.createElement(Footer, {}), document.getElementById('footer'));
