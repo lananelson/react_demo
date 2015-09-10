8ce96e1d7a8d082f2e827217a25ced5aa3bc4ef9
Lana Nelson <lana@mediasilo.com>
2015-09-10 00:21:19 -0400

    v5

--- hello_world/webpack.config.js
+++ hello_world/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
-                loaders: ['babel'],
+                loaders: ['react-hot', 'babel'],
                 exclude: /node_modules/
             }
         ]
