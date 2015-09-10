b9f1d8b25e8bca7947aed1347af2597c76a2cbf7
Lana Nelson <lana@mediasilo.com>
2015-09-10 00:16:06 -0400

    v4

--- hello_world/webpack.config.js
+++ hello_world/webpack.config.js
@@ -1,12 +1,23 @@
 var path = require('path');
+var webpack = require('webpack');
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        path.resolve(__dirname, 'app/main.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:3000/build/'
     },
+    plugins: [
+
+        // plugin for hot load
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
